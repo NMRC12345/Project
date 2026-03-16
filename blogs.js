124881fb3bd30371a94c886d4b7ec4d47174e480
Nitin kumar <nitin.kumar@ptpl.co.in>
2026-03-16 17:26:09 +0530

    first commit

---
+++ blogs.js
@@ -0,0 +1,18 @@
+const blogs = [
+    {
+        title: "Changing Political Sentiment in Urban India",
+        date: "May 2026",
+        category: "Research Papers, trends and opinion",
+        excerpt: "Urban voter behaviour in India is shifting rapidly due to economic pressure and digital narratives.",
+        file: "blog1.html"
+    },
+
+    {
+        title: "How Data Intelligence Is Transforming Election Campaigns",
+        date: "May 2026",
+        category: "Data Analysis",
+        excerpt: "Modern election campaigns are driven by data, micro-targeting and behavioural insights.",
+        file: "blog2.html"
+    }
+
+];
